798fe8f46d64451ebda458864ca1aa791ba816d6
HangNT169 <HangNT169@fpt.edu.vn>
2024-11-19 13:45:19 +0700

    Buổi 10 : CRUD Finish

--- GD2_VueJS/B9_BaiMauCRUD/src/component/FormMayTinhComponent.vue
+++ GD2_VueJS/B9_BaiMauCRUD/src/component/FormMayTinhComponent.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Form</h1>
+  <form>
+    Ten may tinh: <input type="text" v-model="mayTinh.name" /> <br />
+    Gia: <input type="number" v-model="mayTinh.price" /> <br />
+    Mo ta: <input type="text" v-model="mayTinh.descption" /> <br />
+  </form>
 </template>
+<script setup>
+defineModel('mayTinh', {
+  default: () => ({
+    name: '',
+    price: '',
+    descption: '',
+  }),
+})
+</script>
