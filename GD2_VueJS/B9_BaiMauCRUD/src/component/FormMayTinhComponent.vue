036b6a42c80538b755fbca90fcc1ffdb5df7de1e
HangNT169 <HangNT169@fpt.edu.vn>
2024-11-19 13:48:16 +0700

    Buổi 10 : Finsih CRUD

--- GD2_VueJS/B9_BaiMauCRUD/src/component/FormMayTinhComponent.vue
+++ GD2_VueJS/B9_BaiMauCRUD/src/component/FormMayTinhComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <form>
-    Ten may tinh: <input type="text" v-model="mayTinh.name" /> <br />
+    Ten may tinh: <input type="text" v-model.trim="mayTinh.name" /> <br />
     Gia: <input type="number" v-model="mayTinh.price" /> <br />
     Mo ta: <input type="text" v-model="mayTinh.descption" /> <br />
   </form>
